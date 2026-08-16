e1270acc1655ada26316688f232f7a58ff19bcea
nickelmagnet <nickelmagnet@gmail.com>
2026-08-16 23:47:28 +0530

    add help() to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <string>
 
-int main() {
-    std::cout << "ATLAS — A Tiny Little Assistant System\n";
-    std::cout << "Online.\n";
+void help() {
+	std::cout << "ATLAS\nA Tiny Little Operating System\n";
+	std::cout << "Usage: atlas <command>[options][arguments]\n";
+	std::cout << "I haven't decided more lol " << "\n";
+}
 
+int main(int argc, char* argv[]) {
+    if (argc == 1) {
+		help();
+	}
+	else if (argc == 2) {
+		if( std::string(argv[1]) == "--help" || std::string(argv[1]) == "-h" ) {
+			help();
+		}
+	}
+	else {
+		std::cout << "Work in progress.\n";
+	}
     return 0;
 }
 
