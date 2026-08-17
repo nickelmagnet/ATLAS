be873d828f02c2abd95527a24be0f6c2c47ac305
nickelmagnet <nickelmagnet@gmail.com>
2026-08-17 17:52:53 +0530

    Install vcpkg,sqlite

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <string>
 
+void add(std::string task) {
+	std::cout << "Adding task: " << task << "\n";
+	
+}
+
 void help() {
-	std::cout << "ATLAS\nA Tiny Little Operating System\n";
+	std::cout << "ATLAS\nA Tiny Little Assistant System\n";
 	std::cout << "Usage: atlas <command>[options][arguments]\n";
 	std::cout << "I haven't decided more lol " << "\n";
 }
@@ -11,13 +16,21 @@ int main(int argc, char* argv[]) {
     if (argc == 1) {
 		help();
 	}
-	else if (argc == 2) {
+	else if (argc >= 2) {
 		if( std::string(argv[1]) == "--help" || std::string(argv[1]) == "-h" ) {
 			help();
 		}
-	}
-	else {
-		std::cout << "Work in progress.\n";
+		else if (std::string(argv[1]) == "--version" || std::string(argv[1]) == "-v" ) {
+			std::cout << "version 0.0.1\n";
+		}
+		else if (std::string(argv[1]) == "add") {
+			if (argc == 3) {
+				std::string task = argv[2];
+				add(task);
+			}
+			else std::cout << "Give me a task dumbass.\n";
+
+		}
 	}
     return 0;
 }
